35c9642147e6b22c178ac37cf2511bb276b248bf
Gabriel Dias Rezende Martins <gabriel.dias.rezende.martins@gmail.com>
2017-05-24 18:18:44 -0300

    Add navbar layout

--- imports/startup/client/material-design.js
+++ imports/startup/client/material-design.js
@@ -1,3 +1,3 @@
 import '/node_modules/material-design-lite/material.min.js';
-import '/imports/ui/stylesheets/material-icons.css';
 import '/node_modules/material-design-lite/material.min.css';
+import '/imports/ui/stylesheets/material-icons.css';
--- imports/ui/layouts/base-layout.js
+++ imports/ui/layouts/base-layout.js
@@ -2,13 +2,34 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { render } from 'react-dom';
 import { Component } from 'react';
+import styles from '/imports/ui/stylesheets/base-layout'
 
 export default class BaseLayout extends Component {
+    toggleDrawer(event) {
+        event.preventDefault();
+        document.getElementsByClassName('drawer')[0].classList.toggle('active');
+    };
+
     render() {
         return (
-            <div>
-                <h1>layout</h1>
-                <div>{this.props.children}</div>
+            <div className="layout">
+                <div className="navbar">
+                    <i className="menu-icon material-icons" onClick={this.toggleDrawer}>menu</i>
+                    <img className="logo" src="/images/logo_white.png"/>
+                    <div className="vertical-divider"></div>
+                    <div className="title">(Current Page)</div>
+                    <div className="search-bar">
+                        <i className="search-icon material-icons">search</i>
+                        <input className="search-input" type="search" placeholder="Search"/>
+                    </div>
+                    <div className="auth-status">(Auth Status)</div>
+                </div>
+                <div className="page-content">
+                    <div className="drawer">
+
+                    </div>
+                    {this.props.children}
+                </div>
             </div>
         )
     }
